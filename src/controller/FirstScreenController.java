3e46cbfdd86fdaa05872c3ebe790b9c5fa22518c
cello <cello.ma16@gmail.com>
2020-02-25 16:52:36 -0300

    a

---
+++ src/controller/FirstScreenController.java
@@ -0,0 +1,62 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+
+public class FirstScreenController {
+
+    @FXML
+    private Label lblHeader;
+
+    @FXML
+    private ImageView imgHeader;
+
+    @FXML
+    private Label lblJogadores;
+
+    @FXML
+    private Button btnAdcJogador;
+
+    @FXML
+    private Button btnListJogador;
+
+    @FXML
+    private Label lblPartidas;
+
+    @FXML
+    private Button btnAdcPartidas;
+
+    @FXML
+    private Button btnListPartidas;
+
+    @FXML
+    private Label lblLastGame;
+
+    @FXML
+    private Label lblTime;
+
+    @FXML
+    private ImageView imgGame;
+
+    @FXML
+    private Label lblPlacar1;
+
+    @FXML
+    private Label lblVs;
+
+    @FXML
+    private Label lblTime2;
+
+    @FXML
+    private ImageView imgGame2;
+
+    @FXML
+    private Label lblPlacar2;
+
+    @FXML
+    private Button btnVerPartidas;
+
+}
